720b1631b483247bb3d9695321fb2669b6626d54
Yan de Paula <yan.paula@shipp.delivery>
2021-03-21 18:52:07 -0300

    PSR 12 adjustments

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -39,6 +39,5 @@ class AuthController extends Controller
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
 
         return response(['user' => auth()->user(), 'access_token' => $accessToken]);
-
     }
 }
